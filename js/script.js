fba9f7f403f2e88c2c32701bb1e9b617cd193ed1
KhaledSwidan <khaledqutb4@gmail.com>
2022-01-15 17:09:19 +0200

    add loading

--- js/script.js
+++ js/script.js
@@ -196,3 +196,22 @@ btnSeeMoreCls.addEventListener("click", () =>
 {
   listSeeMore.style.display = "none";
 })
+
+// landing pg;
+const pg = document.querySelector(".pg");
+const lodTxt = document.querySelector(".loadTxt");
+let load = 0;
+let int = setInterval(blurring, 30);
+function blurring()
+{
+  load++;
+  if (load > 99) { clearInterval(int); };
+  lodTxt.innerHTML = `${load}%`;
+  lodTxt.style.opacity = scale(load, 0, 100, 1, 0);
+  pg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+function scale(number, inMin, inMax, outMin, outMax)
+{
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
